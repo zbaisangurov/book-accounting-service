4a02075d8e2f3cccf567942a6d79eadb32495e94
Zaurbek Baisangurov <baisangurov.zaur@gmail.com>
2025-06-20 19:54:34 +0200

    JV-44 feat: Initial commit
    Инициализирован репозиторий, загружена базовая структура проекта.

---
+++ src/main/java/com/kode/bookaccountingservice/BookAccountingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kode.bookaccountingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAccountingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookAccountingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kode/bookaccountingservice/BookAccountingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kode.bookaccountingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAccountingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
