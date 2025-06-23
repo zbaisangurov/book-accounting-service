9ced885243c7a74c5960429898c1d3c0b1a06f5a
Zaurbek Baisangurov <baisangurov.zaur@gmail.com>
2025-06-23 11:03:07 +0200

    JV-44 fix: Исправлена проблема с запуском проекта
    Переменные окружения .env подтянуты в тестовый класс.

--- src/test/java/com/kode/bookaccountingservice/BookAccountingServiceApplicationTests.java
+++ src/test/java/com/kode/bookaccountingservice/BookAccountingServiceApplicationTests.java
@@ -1,10 +1,19 @@
 package com.kode.bookaccountingservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 class BookAccountingServiceApplicationTests {
+    @BeforeAll
+    static void loadEnv() {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue()));
+    }
 
     @Test
     void contextLoads() {
